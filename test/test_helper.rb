028847e4d0efa9790726b905ac10bf27eaa332ff
Jean Joeris <joeris.peter@gmail.com>
2016-08-23 14:54:33 -0600

    Initial commit - copying over from skeleton repo, with a few tests

---
+++ test/test_helper.rb
@@ -0,0 +1,15 @@
+ENV["RACK_ENV"] ||= "test"
+
+
+require 'bundler'
+Bundler.require
+
+require File.expand_path("../../config/environment", __FILE__)
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'capybara/dsl'
+require 'pry'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+Capybara.app = RushHour::Server
