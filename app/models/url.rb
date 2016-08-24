0a9c5e194adc28a0697ce196ebc12d756ee97e49
Jean Joeris <joeris.peter@gmail.com>
2016-08-24 17:48:29 -0600

    max time and http verbs for a specific url, working with tests

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,4 +3,15 @@ class Url < ActiveRecord::Base
 
   validates :url_path, presence: true
 
+  def max_response_time
+    payload_requests.max_response_time
+  end
+
+  def http_verbs
+    request_types = payload_requests.select("request_type_id")
+    request_types.map do |request_type|
+      RequestType.find(request_type.request_type_id).http_verb
+    end
+  end
+
 end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -42,4 +42,80 @@ class UrlsTest < Minitest::Test
     assert_equal payload, url.payload_requests.first
   end
 
+  def test_url_can_find_max_response_time_of_all_payloads
+    payload_data_1 = get_payload_data
+
+    payload_data_2 = get_payload_data
+    payload_data_2["responded_in"] = 20
+
+    payload_data_3 = get_payload_data
+    payload_data_3["responded_in"] = 200
+
+    PayloadRequest.create(payload_data_1)
+    PayloadRequest.create(payload_data_2)
+    PayloadRequest.create(payload_data_3)
+    url = Url.create(url_path: "http://www.google.com")
+
+    assert_equal 200, url.max_response_time
+  end
+
+  def test_url_max_response_time_only_looks_at_correct_payloads
+    payload_data_1 = get_payload_data
+
+    payload_data_2 = get_payload_data
+    payload_data_2["responded_in"] = 20
+
+    payload_data_3 = get_payload_data
+    payload_data_3["responded_in"] = 200
+    payload_data_3["url_id"] = 2
+
+    PayloadRequest.create(payload_data_1)
+    PayloadRequest.create(payload_data_2)
+    PayloadRequest.create(payload_data_3)
+    google = Url.create(url_path: "http://www.google.com")
+    reddit = Url.create(url_path: "http://www.reddit.com")
+
+    assert_equal 20, google.max_response_time
+  end
+
+  def test_url_finds_all_verbs_associated_with_it
+    populate_request_types_table
+
+    payload_data_1 = get_payload_data
+    payload_data_2 = get_payload_data
+    payload_data_2["request_type_id"] = 2
+    payload_data_3 = get_payload_data
+    payload_data_3["request_type_id"] = 3
+
+    PayloadRequest.create(payload_data_1)
+    PayloadRequest.create(payload_data_2)
+    PayloadRequest.create(payload_data_3)
+
+    google = create_params
+
+    assert_equal ["GET", "POST", "PUT"], google.http_verbs
+  end
+
+  def test_url_does_not_find_verbs_not_associated_with_it
+    populate_request_types_table
+
+    payload_data_1 = get_payload_data
+
+    payload_data_2 = get_payload_data
+    payload_data_2["request_type_id"] = 2
+
+    payload_data_3 = get_payload_data
+    payload_data_3["request_type_id"] = 3
+    payload_data_3["url_id"] = 2
+
+    PayloadRequest.create(payload_data_1)
+    PayloadRequest.create(payload_data_2)
+    PayloadRequest.create(payload_data_3)
+
+    google = create_params
+    reddit = Url.create(url_path: "http://reddit.com")
+    
+    assert_equal ["GET", "POST"], google.http_verbs
+  end
+
 end
