b7ccaa82c4e0fd01fa8c0e700ae0ea4e86c606bf
Jean Joeris <joeris.peter@gmail.com>
2016-08-23 18:44:13 -0600

    updated payload_request model and test to conform to new schema

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -1,15 +1,10 @@
 class PayloadRequest < ActiveRecord::Base
-  validates :url, presence: true
   validates :requested_at, presence: true
   validates :responded_in, presence: true
-  validates :referred_by, presence: true
-  validates :request_type, presence: true
-  validates :user_agent, presence: true
-  validates :resolution_width, presence: true
-  validates :resolution_height, presence: true
-  validates :ip, presence: true
-
-  def self.format_and_create(data)
-    #format the data
-    self.create(data)
+  validates :ip_id, presence: true
+  validates :url_id, presence: true
+  validates :referrer_id, presence: true
+  validates :user_agent_id, presence: true
+  validates :screen_resolution_id, presence: true
+  validates :request_type_id, presence: true
 end
